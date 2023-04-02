758920d298b2915c34a2361addc80e084edba0cc
edusebass <eduardoalmachi123@hotmail.com>
2023-04-02 18:38:34 -0500

    update

---
+++ teory/js_objects.js
@@ -0,0 +1,10 @@
+//java object is a un  value that contains multiple values in the same.
+var houseKeeper = {
+    yearsOfexperience: 12,
+    name: "Jane",
+    cleaningRepertoire: ["bathroom", "lobby", "bedroom"]
+}
+
+//call to variable
+//thats is a piece information with valor associated
+console.log(houseKeeper.name);
