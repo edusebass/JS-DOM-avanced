e1d4e416b894a62cb9cd14e530e784791e8671cb
edusebass <eduardoalmachi123@hotmail.com>
2023-04-03 20:25:34 -0500

    update

--- teory/js_objects.js
+++ teory/js_objects.js
@@ -8,3 +8,12 @@ var houseKeeper = {
 //call to variable
 //thats is a piece information with valor associated
 console.log(houseKeeper.name);
+
+//create constructor and call and variable
+function HouseKeeper (yearsOfExperience, name, cleaningRepertoire) {
+    this.yearsOfExperience = years;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
+}
+//call the new constructor
+var houseKeeper1 = new HouseKeeper(12, "Edu", ["lobby", "bathroom"]);
