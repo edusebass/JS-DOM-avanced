befcf3c5c1de95ce7bae8622f4b5f4c0e6c4f6a3
edusebass <eduardoalmachi123@hotmail.com>
2023-04-08 21:48:45 -0500

    update

--- index.js
+++ index.js
@@ -5,51 +5,82 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 //selecting the buttons witch boocle for and calling function for click events
 for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
         //Add sounds on a website
-        var buttonInnerHTML = this.innerHTML;
+        const buttonInnerHTML = this.innerHTML;
+
         makeSound(buttonInnerHTML);
-        
+
+        buttonAnimation(buttonInnerHTML);
+
     });
 }
 
-//Detecting keyboard press events
-
-document.addEventListener("keydown", function(event) {
-    makeSound(event.key);
-});
-
+//sounds 
 function makeSound(key) {
     switch (key) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();  
             break;
+
         case "a":
             var tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();  
             break;
+
         case "s":
             var tom3 = new Audio("sounds/tom-3.mp3");
             tom3.play();  
             break;
+
         case "d":
             var tom4 = new Audio("sounds/tom-4.mp3");
             tom4.play();  
             break;
+
         case "j":
             var snare = new Audio("sounds/snare.mp3");
             snare.play();  
             break;
+            
         case "k":
             var crash = new Audio("sounds/crash.mp3");
             crash.play();  
             break;
+            
         case "l":
             var kick = new Audio("sounds/kick-bass.mp3");
             kick.play();  
             break;
-        default:
-            break;
+
+
+        default: console.log(buttonInnerHTML);
     }
 }
+
+//Detecting keyboard press events
+
+document.addEventListener("keydown", function(event) {
+
+    makeSound(event.key);
+
+});
+
+//animation function 
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 150);
+}
+
+document.addEventListener("keydown", function(event) {
+
+    buttonAnimation(event.key);
+
+});
 
